ac1949b3819f822fa6a90036a9b727a65fd4be7b
Rodney <54865844+rmartin-sc@users.noreply.github.com>
2023-03-27 20:19:32 -0400

    Some day maybe mediapipe will work...

--- hand-detection-worker.js
+++ hand-detection-worker.js
@@ -12,12 +12,17 @@ onmessage = async (event) => {
                             "//cdn.jsdelivr.net/npm/@tensorflow/tfjs-converter",
                             "//cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-webgl",
                             "//cdn.jsdelivr.net/npm/@tensorflow-models/hand-pose-detection");
+            // importScripts("//cdn.jsdelivr.net/npm/@mediapipe/hands",
+            //                 "//cdn.jsdelivr.net/npm/@tensorflow/tfjs-core",
+            //                 "//cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-webgl",
+            //                 "//cdn.jsdelivr.net/npm/@tensorflow-models/hand-pose-detection");
             console.log("...done loading tensorflow scripts");
 
             console.log("Initializing tensorflow model...");
             const model = handPoseDetection.SupportedModels.MediaPipeHands;
             const detectorConfig = {
-                runtime: 'tfjs',
+                runtime: 'tfjs', // 'mediapipe',
+                // solutionPath: '//cdn.jsdelivr.net/npm/@mediapipe/hands',
                 modelType: 'full',
             };
             detector = await handPoseDetection.createDetector(model, detectorConfig);
